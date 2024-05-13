7d0e85edbdbdee55b3ac7973a0cc24c84b10f424
leventcorapsiz <leventcorapsiz@hotmail.com>
2024-05-13 15:16:05 +0300

    parameter type

--- src/Data/Data.php
+++ src/Data/Data.php
@@ -6,7 +6,7 @@ namespace Leventcz\Top\Data;
 
 abstract class Data
 {
-    public static function fromArray($attributes): static
+    public static function fromArray(array $attributes): static
     {
         return new static(...$attributes);
     }
