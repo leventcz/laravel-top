5514c4dcf7ab2e876e61d52f3e20347274c5b9dc
leventcorapsiz <leventcorapsiz@hotmail.com>
2024-05-13 12:45:58 +0300

    facade for state

---
+++ src/Facades/State.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Leventcz\Top\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use Leventcz\Top\Data\HandledRequest;
+use Leventcz\Top\StateManager;
+
+/**
+ * @method static StateManager add(string $event, int $times = 1)
+ * @method static void save(HandledRequest $request)
+ */
+class State extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return 'top.state';
+    }
+}
