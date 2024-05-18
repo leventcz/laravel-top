99652f4d2a234aa2edb01dc1b068b35eb1556be7
leventcorapsiz <leventcorapsiz@hotmail.com>
2024-05-18 23:40:05 +0300

    code styling

--- tests/ServiceProvider.php
+++ tests/ServiceProvider.php
@@ -34,7 +34,8 @@ it('ensures top.state bound to the container', function () {
 it('ensures top is always singleton', function () {
     $top = $this->app->get('top');
 
-    expect($this->app->get('top'))->toBe($top);
+    expect($this->app->get('top'))
+        ->toBe($top);
 });
 
 it('ensures top facade resolves correctly', function () {
@@ -47,5 +48,6 @@ it('ensures top facade resolves correctly', function () {
 it('ensures state facade resolves correctly', function () {
     State::setFacadeApplication($this->app);
 
-    expect(State::getFacadeRoot())->toBeInstanceOf(StateManager::class);
+    expect(State::getFacadeRoot())
+        ->toBeInstanceOf(StateManager::class);
 });
