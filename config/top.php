afb8e5634257f7bf21b354aadba0973e6709678f
leventcorapsiz <leventcorapsiz@hotmail.com>
2024-05-26 19:18:59 +0300

    configure recording mode

--- config/top.php
+++ config/top.php
@@ -1,8 +1,32 @@
 <?php
 
 return [
+
     /*
-     * Provide a redis connection from config/database.php
+    |--------------------------------------------------------------------------
+    | Connection
+    |--------------------------------------------------------------------------
+    |
+    | Specify the Redis database connection from config/database.php
+    | that Top will use to save data.
+    | The default value is suitable for most applications.
+    |
     */
+
     'connection' => env('TOP_REDIS_CONNECTION', 'default'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Recording Mode
+    |--------------------------------------------------------------------------
+    |
+    | Determine when Top should record application metrics based on this value.
+    | By default, Top only listens to your application when it is running.
+    | If you want to access metrics through the facade, you can select the 'always' mode.
+    |
+    | Available Modes: "while_running", "always"
+    |
+    */
+
+    'recording_mode' => env('TOP_RECORDING_MODE', 'while_running'),
 ];
