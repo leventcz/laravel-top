51c4a719c68fd4faec4aea1fbbd9e1e84dcebc2e
leventcorapsiz <leventcorapsiz@hotmail.com>
2024-05-26 16:51:03 +0300

    force recording while command is running

--- src/Commands/TopCommand.php
+++ src/Commands/TopCommand.php
@@ -32,12 +32,14 @@ class TopCommand extends Command
             $guiBuilder
                 ->exitAlternateScreen()
                 ->showCursor();
-            exit();
+            exit(1);
         });
     }
 
     private function feed(GuiBuilder $guiBuilder): void
     {
+        Top::startRecording();
+
         $guiBuilder
             ->setRequestSummary(Top::http())
             ->setDatabaseSummary(Top::database())
